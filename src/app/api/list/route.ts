a11224c9561df4f8c0997f822b942a42d15150c8
Jefferson Shibuya <jeffersonshibuya@yahoo.com.br>
2023-08-23 11:10:30 -0300

    Initial

--- src/app/api/list/route.ts
+++ src/app/api/list/route.ts
@@ -1,13 +1,32 @@
 import { NextResponse } from "next/server";
+import { DynamoDBClient, ScanCommand } from '@aws-sdk/client-dynamodb';
+import { unmarshall } from '@aws-sdk/util-dynamodb';
 
+const client = new DynamoDBClient({
+  region: process.env.NEXT_AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.NEXT_AWS_ACCESS_KEY_ID || '',
+    secretAccessKey: process.env.NEXT_AWS_SECRET_ACCESS_KEY || ''
+  }
+});
 export async function POST() {
   try {
-    return NextResponse.json([
-      { id: 1, value: 'test01'},
-      { id: 2, value: 'test02'},
-      { id: 3, value: 'test03'},
-      { id: 4, value: 'test04'},
-    ]);
+    const input = {
+      TableName: process.env.NEXT_AWS_DYNAMODB_TABLE_NAME,
+      FilterExpression: '#status <> :statusValue',
+      ExpressionAttributeNames: {
+        '#status': 'status'
+      },
+      ExpressionAttributeValues: {
+        ':statusValue': { S: 'uploading' }
+      }
+    };
+    const command = new ScanCommand(input);
+    const response = await client.send(command);
+
+    const items = response.Items?.map((item) => unmarshall(item));
+
+    return NextResponse.json(items);
   } catch (error) {
     console.log(error);
   }
