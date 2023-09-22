cf8f4d1426680bc04283994611d403dfdf0a7cb9
Jefferson Shibuya <jeffersonshibuya@yahoo.com.br>
2023-09-22 13:37:14 -0300

    Downgrade nextjs to v.13.3.4

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "api": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "testnextdeploy2b33e9b6": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
