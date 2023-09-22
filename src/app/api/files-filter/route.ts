131ca07dffe3d713f1be74e3144053efb3b04c3a
Jefferson Shibuya <jeffersonshibuya@yahoo.com.br>
2023-09-22 13:04:54 -0300

    Added Public URL

--- src/app/api/files-filter/route.ts
+++ src/app/api/files-filter/route.ts
@@ -12,27 +12,32 @@ const ddbClient = new DynamoDBClient({
 });
 
 export async function POST(request: Request) {
-  const { year } = await request.json();
-
-  let filterExpression = '#year = :yearValue AND #isPublic = :isPublicValue';
-  const expressionValues: { [key: string]: any } = {
-    ':yearValue': { S: year },
-    ':isPublicValue': { BOOL: true }
+  try {
+    
+    const { year } = await request.json();
+  
+    let filterExpression = '#year = :yearValue AND #isPublic = :isPublicValue';
+    const expressionValues: { [key: string]: any } = {
+      ':yearValue': { S: year },
+      ':isPublicValue': { BOOL: true }
+    }
+  
+    const input = {
+      TableName: process.env.NEXT_AWS_DYNAMODB_TABLE_NAME,
+      FilterExpression: filterExpression,
+      ExpressionAttributeNames: {
+        '#year': 'year',
+        '#isPublic': 'isPublic',
+      },
+      ExpressionAttributeValues: expressionValues
+    };
+  
+    const command = new ScanCommand(input);
+    const response = await ddbClient.send(command);
+    const items = response.Items?.map((item) => unmarshall(item)) || [];
+  
+    return NextResponse.json(items);
+  } catch (error) {
+    return NextResponse.error()
   }
-
-  const input = {
-    TableName: process.env.NEXT_AWS_DYNAMODB_TABLE_NAME,
-    FilterExpression: filterExpression,
-    ExpressionAttributeNames: {
-      '#year': 'year',
-      '#isPublic': 'isPublic',
-    },
-    ExpressionAttributeValues: expressionValues
-  };
-
-  const command = new ScanCommand(input);
-  const response = await ddbClient.send(command);
-  const items = response.Items?.map((item) => unmarshall(item)) || [];
-
-  return NextResponse.json(items);
 }
